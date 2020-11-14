ae587473e5d9feeb30eaafb9e844b4878447e39d
Andrey Kasimovskiy <kasimovskiy.a.d.public@gmail.com>
2020-11-14 21:12:42 +0300

    Add task 2 - body mass index

---
+++ Home_work_1/Task2.BodyMassIndex/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.BodyMassIndex
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello! Let's calculate your body mass index.");
+
+			//Enter height
+			Console.Write("Enter your height (in metres): ");
+			var userHeight = Convert.ToDouble(Console.ReadLine());
+
+			//Enter weight
+			Console.Write("Enter your weight: ");
+			var userWeight = Convert.ToDouble(Console.ReadLine());
+
+			var bodyMassIndex = userWeight / (userHeight * userHeight);
+
+			Console.WriteLine($"You body mass index: {bodyMassIndex}.");
+		}
+	}
+}
