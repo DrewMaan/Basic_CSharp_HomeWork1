7272e8428836e8028613cf92f6ad246a96c06da8
Andrey Kasimovskiy <kasimovskiy.a.d.public@gmail.com>
2020-11-14 22:51:00 +0300

    Add task 4 - swap variables

---
+++ Home_work_1/Task4.SwapVariables/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.SwapVariables
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//paragraph A
+			int number1 = 468;
+			int number2 = 135;
+			Console.WriteLine($"Original data: {number1} and {number2}.");
+
+			int temp = number1;
+			number1 = number2;
+			number2 = temp;
+			Console.WriteLine($"Result data: {number1} and {number2}.");
+
+			//paragraph B
+			number1 = 468;
+			number2 = 135;
+			Console.WriteLine($"Original data: {number1} and {number2}.");
+
+			number1 ^= number2;
+			number2 ^= number1;
+			number1 ^= number2;
+			Console.WriteLine($"Result data: {number1} and {number2}.");
+		}
+	}
+}
